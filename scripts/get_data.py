75259d67bb47299fd540fc9868e74c93b5ba2f61
Ковалець Владислав Миколайович <146095962+kovalets-vlad@users.noreply.github.com>
2026-03-01 17:25:12 +0200

    Update continuous integration (#12)
    * fix: improve CI pipeline conditions and cleanup steps
    
    * chore: add comment for CI testing in get_data.py
    
    * feat: enhance CI pipeline to support multiple modules and improve artifact handling
    
    * fix: update data source path in sample_analysis.py to use equipment_data.csv
    
    * fix: update save paths for hypothesis analysis plots in sample_analysis.py
    
    ---------
    
    Co-authored-by: kovalets-vlad <vladyslav.kovalets.shi.2023@gmail.com>

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -2,7 +2,7 @@ import os
 import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
-
+# тестовий запуск CI
 def download_data():
     url = "https://data.gov.ua/dataset/41cb520d-95d8-4c5c-baad-9d4177618ace/resource/400a1681-6535-43fa-b76c-d27efc348773/download/equipment_2025-12-15.csv" 
     save_path = "open-data-ai-analytics/data/raw/equipment_data.csv"
